a4b4f5a5a06a7d9c504b27bbea3eacdc3a76ea34
DINOSH-M <dinooss2005@gmail.com>
2024-11-21 01:02:39 +0530

    Describe the HappyNumber.java

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,3 +1,16 @@
+// A happy number is a number that, when you repeatedly sum the squares of its digits,
+// eventually reaches 1. If it does not reach 1 and instead falls into a cycle of numbers, 
+//it is considered unhappy or sad.
+
+//EX : n = 7 -> 7^2 = 49 
+//-> 4^2 + 9^2 = 16 + 81 = 97 
+//-> 9^2 + 7^2 = 81 + 49 = 130 
+//-> 1^2 + 3^2 + 0^2 = 1 + 9 + 0 = 10 
+//-> 1^2 + 0 ^2 = 1
+
+// suppose there exist any repeated element n then it is not a happy number.
+
+
 package com.numberTheory;
 
 import java.util.HashSet;
